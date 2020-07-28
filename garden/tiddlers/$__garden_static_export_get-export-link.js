cd5cef5d3020cea881eeb9aeb206dfb27c7410f9
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-07-28 23:54:32 +0200

    Add garden tiddlywiki project folder from nodejs
    This is the first iteration of the tiddlywiki already containing
    two pages: welcome and implementing a spaced repetition writing
    system.
    
    It will be deployed using Travis.

---
+++ garden/tiddlers/$__garden_static_export_get-export-link.js
@@ -0,0 +1,24 @@
+(function(){
+"use strict";
+
+exports.name = "tv-get-export-link";
+
+exports.params = [
+];
+
+exports.run = function() {
+    var title = this.to;
+    var sanitized_title = title.replace(/([^a-z0-9]+)/gi, '-');
+    var attr = this.getVariable("tv-subfolder-links");
+    var path_to_root="./"	
+    var finalLink=path_to_root
+
+    
+    var wikiLinkTemplateMacro = this.getVariable("tv-wikilink-template"),
+        wikiLinkTemplate = wikiLinkTemplateMacro ? wikiLinkTemplateMacro.trim() : "#$uri_encoded$",
+        wikiLinkText = wikiLinkTemplate.replace("$uri_encoded$",encodeURIComponent(sanitized_title));	
+    wikiLinkText = wikiLinkText.replace("$uri_doubleencoded$",encodeURIComponent(sanitized_title));
+    return (finalLink + wikiLinkText).toLocaleLowerCase();
+};
+
+})();
